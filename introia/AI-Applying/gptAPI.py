fb8e631d1a4d9787981d37d9a5c9c1a95ef0b81b
João Gabriel <joaogabriel9633@gmail.com>
2025-06-19 19:59:44 -0300

    Added secrect key

--- introia/AI-Applying/gptAPI.py
+++ introia/AI-Applying/gptAPI.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 import os
 
+os.environ['OPENAI_API_KEY'] = "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"
 
 client = OpenAI()
 
